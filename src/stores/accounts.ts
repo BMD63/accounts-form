554c6731f2c616d2d150bdbdb153f95928566fb2
Sergei <chechet@mail.ru>
2025-09-22 11:51:09 +0400

    store types added, store changed

--- src/stores/accounts.ts
+++ src/stores/accounts.ts
@@ -1,11 +1,32 @@
 import { defineStore } from 'pinia';
+import { ref } from 'vue';
+import type { Account } from '../types/account';
 
-export interface Account {
-    id: string;
+function genId(): string {
+  return 'acc_' + Math.random().toString(36).slice(2, 10) + Date.now().toString(36);
 }
 
-export const useAccountsStore = defineStore('accounts', {
-  state: () => ({
-    accounts: [] as Account[],
-  }),
+export const useAccountsStore = defineStore('accounts', () => {
+  const accounts = ref<Account[]>([]);
+
+  function addEmpty() {
+    const acc: Account = {
+      id: genId(),
+      labels: [],
+      type: 'LOCAL',
+      login: '',
+      password: '',
+    };
+    accounts.value.push(acc);
+  }
+
+  function remove(id: string) {
+    accounts.value = accounts.value.filter(a => a.id !== id);
+  }
+
+  return {
+    accounts,
+    addEmpty,
+    remove,
+  };
 });
